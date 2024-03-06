68ddd559a7049c3ab4d367481c23bd672ac20c78
mmaletskov <maleckovmihail@gmail.com>
2024-03-06 16:08:39 +0300

    pr7

--- project_2/src/components/Catalog/Catalog.jsx
+++ project_2/src/components/Catalog/Catalog.jsx
@@ -2,20 +2,34 @@ import './Catalog.css';
 import CatalogCategories from './CatalogCategories/CatalogCategories';
 import CatalogItem from './CatalogItem/CatalogItem';
 import { catalog } from '../../../database';
+import { useState } from 'react';
 
 export default function Catalog(){
+
+    const[searchQuery,setSearchQuery] = useState("");
+
+    const searchProducts = catalog.filter(
+        (item) => item.name.toLowerCase().includes(searchQuery)
+    )
+    
     return(
         <>
         <section className="catalog">
             <div className="container">
                 <div className="catalog__inner">
                     <CatalogCategories />
+                    <input type="search" placeholder="Поиск" onChange={(e) =>setSearchQuery(e.target.value) } />
                   <div className="catalog__list">
-                        {catalog.map((card,index) =>{
+                        {
+                        searchProducts.length ?    
+                        searchProducts.map((card,index) =>{
                             return(
                                 <CatalogItem key={index} {...card}/>
                             )
-                        })}
+                        })
+                    :
+                    <p>По запросу {searchQuery} ничего не найдено</p>
+                    }
                   </div>
                 </div>
             </div>
